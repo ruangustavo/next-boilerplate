4a16d8d60df234442dca18b32cba107cea0b715b
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2024-04-07 00:50:12 -0300

    feat: init project

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <header className="border-b py-4">
+        <div className="container">
+          <h1 className="font-semibold text-xl tracking-tight">To-do app</h1>
+        </div>
+      </header>
+      <main className="flex-1 pt-4 container space-y-8"></main>
+    </>
+  )
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+import { cn } from '@/lib/utils'
+import { siteConfig } from '@/config/site'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: siteConfig.title,
+  description: siteConfig.description,
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={cn(inter.className, 'antialiased flex flex-col min-h-svh')}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
