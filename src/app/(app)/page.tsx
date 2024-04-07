ed79b43f23d18d015c167de170622874348f8380
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2024-04-07 00:53:20 -0300

    feat: update page title in Home component

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,13 @@
+import { siteConfig } from '@/config/site'
+
 export default function Home() {
   return (
     <>
       <header className="border-b py-4">
         <div className="container">
-          <h1 className="font-semibold text-xl tracking-tight">To-do app</h1>
+          <h1 className="font-semibold text-xl tracking-tight">
+            {siteConfig.title}
+          </h1>
         </div>
       </header>
       <main className="flex-1 pt-4 container space-y-8"></main>
