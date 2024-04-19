1c85d03ffd8276ed7835c844b601ef2bdbc334ff
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2024-04-19 18:11:28 -0300

    feat: add TailwindIndicator component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
 import { siteConfig } from '@/config/site'
+import { TailwindIndicator } from '@/components/tailwind-indicator'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <TailwindIndicator />
         </ThemeProvider>
       </body>
     </html>
